34b729bb9b339759752f705e44ce3381717dd900
rlanman14 <ryan.lanman@tamu.edu>
2023-09-03 19:31:50 -0500

    first commit

---
+++ ryanjs/words.js
@@ -0,0 +1,59 @@
+/*word&hint library*/
+let words = [
+    {
+        word: "redass",
+        hint: "very enthusiastic aggie"
+    },
+    {
+        word: "muster",
+        hint: "yearly memorial event for passed aggies"
+    },
+    {
+        word: "bonfire",
+        hint: "aggie football pre-grame tradition, has memorial"
+    },
+    {
+        word: "howdy",
+        hint: "the aggie greeting"
+    },
+    {
+        word: "twelfth",
+        hint: "_______ man"
+    },
+    {
+        word: "aggieland",
+        hint: "the best place on earth"
+    },
+    {
+        word: "badbull",
+        hint: "anything against aggie spirit"
+    },
+    {
+        word: "centurytree",
+        hint: "romantic walk under"
+    },
+    {
+        word: "reveille",
+        hint: "A&M's first lady"
+    },
+    {
+        word: "whoop",
+        hint: "only said by third and fourth years"
+    },
+    {
+        word: "cadets",
+        hint: "the corps of ..."
+    },
+    {
+        word: "hullabaloo",
+        hint: "__________! Canek! Canek!"
+    },
+    {
+        word: "maroon",
+        hint: "aggie color"
+    },
+    {
+        word: "jimbo",
+        hint: "aggie football coach"
+    },
+]
